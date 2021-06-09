c04e4154a03c16167aa9197184f2f2896e604cce
Szymon Rojek <sz.rojek@gmail.com>
2021-06-10 00:45:46 +0100

    add Form component to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import './App.css';
 import Container from "./Container";
 import Header from "./Header";
 import Footer from "./Footer";
+import Form from "./Form";
 
 function App() {
   return (
     <Container>
       <Header title="Currency Converter" />
+      <Form />
       <Footer text="© 2021 | Coded by Szymon Rojek" />
     </Container>
   );
