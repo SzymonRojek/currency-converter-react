0e4eb3a9730673cbcc2d7ab62b3b0634d8cded06
Szymon Rojek <sz.rojek@gmail.com>
2021-06-09 23:30:06 +0100

    add Footer component to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Container from "./Container";
 import Header from "./Header";
+import Footer from "./Footer";
 
 function App() {
   return (
     <Container>
       <Header title="Currency Converter" />
+      <Footer text="© 2021 | Coded by Szymon Rojek" />
     </Container>
   );
 }
