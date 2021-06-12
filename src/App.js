af9c3dded72bcf22367bc0b1017acb3ba668abde
Szymon Rojek <sz.rojek@gmail.com>
2021-06-12 19:52:33 +0100

    add currencies attribute to the Form component in the App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import Container from "./Container";
 import Header from "./Header";
 import Footer from "./Footer";
 import Form from "./Form";
+import currencies from "./currencies";
 
 function App() {
   return (
     <>
       <Container>
         <Header title="Currency Converter" />
-        <Form />
+        <Form currencies={currencies} />
         <Footer text="© 2021 | Coded by Szymon Rojek" />
       </Container>
     </> 
