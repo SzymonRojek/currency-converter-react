294a4477dc2690943cd51e9be596d25d465ea421
Szymon Rojek <sz.rojek@gmail.com>
2021-06-13 13:54:08 +0100

    add getIdCurrencyFrom to the Input component

--- src/Form/Input/index.js
+++ src/Form/Input/index.js
@@ -1,25 +1,30 @@
 import './style.css';
 
-const Input = ({ amount, getIdCurrencyFrom, setAmount }) => (
-  <label className="form__label">
-    <span className="form__labelText form__labelText--last">
-      Amount {getIdCurrencyFrom !== "search" ? `in ${getIdCurrencyFrom}` : ""}*:
-    </span>
-    <div className="form__amountVisual">
-      <input
-        value={amount}
-        onChange={({ target }) => setAmount(target.value)}
-        className="form__amountVisual-input" 
-        type="number" 
-        name="amount" 
-        min="1" 
-        step="any" 
-        placeholder="type here" 
-        required
-      />
-      <span className="focus"></span>
-    </div>
-  </label>
-);
+const Input = ({ currencies, amount, setAmount, currencyFrom }) => {
+
+  const getIdCurrencyFrom = currencies.find( ({ name }) => name === currencyFrom).id;
+
+  return (
+    <label className="form__label">
+      <span className="form__labelText form__labelText--last">
+        Amount {getIdCurrencyFrom !== "search" ? `in ${getIdCurrencyFrom}` : ""}*:
+      </span>
+      <div className="form__amountVisual">
+        <input
+          value={amount}
+          onChange={({ target }) => setAmount(target.value)}
+          className="form__amountVisual-input" 
+          type="number" 
+          name="amount" 
+          min="1" 
+          step="any" 
+          placeholder="type here" 
+          required
+        />
+        <span className="focus"></span>
+      </div>
+    </label>
+  );
+};
 
 export default Input;
