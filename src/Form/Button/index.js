ded567278076eb6987d7bef8b5b4262433aa83f0
Szymon Rojek <sz.rojek@gmail.com>
2021-06-13 13:54:53 +0100

    remove unnecessary props from the Button component

--- src/Form/Button/index.js
+++ src/Form/Button/index.js
@@ -1,9 +1,11 @@
 import './style.css';
 
-const Button = ({currencyFrom, currencyTo, title}) => (
-  <div className="form__footer">
-    <button className="form__button" disabled={currencyFrom === currencyTo}>{title}</button>
-  </div>
-);
+const Button = ({ title, disabled }) => {
 
+  return (
+    <div className="form__footer">
+      <button className="form__button" disabled={disabled}>{title}</button>
+    </div>
+  );
+}
 export default Button
