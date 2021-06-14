397d9a88f78e25acdeef93cb7f0b343c88e21285
Szymon Rojek <sz.rojek@gmail.com>
2021-06-14 19:40:06 +0100

    formatting Button component

--- src/Form/Button/index.js
+++ src/Form/Button/index.js
@@ -2,6 +2,11 @@ import './style.css';
 
 export const Button = ({ title, disabled }) => (
   <div className="form__footer">
-    <button className="form__button" disabled={disabled}>{title}</button>
+    <button 
+      className="form__button" 
+      disabled={disabled} 
+    > 
+    {title}
+    </button>
   </div>
 );
