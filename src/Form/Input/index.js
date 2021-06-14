f1eba087308e2da90b543e1899d421e36e1c1402
Szymon Rojek <sz.rojek@gmail.com>
2021-06-14 19:38:35 +0100

    edit Input component: remove variable getIdCurrencyFrom and move to the Form component, change attributes min and step

--- src/Form/Input/index.js
+++ src/Form/Input/index.js
@@ -1,9 +1,7 @@
 import './style.css';
 
-export const Input = ({ currencies, amount, setAmount, currencyFrom }) => {
-
-  const getIdCurrencyFrom = currencies.find( ({ name }) => name === currencyFrom).id;
-
+export const Input = ({ amount, setAmount, inputTypedAmount, getIdCurrencyFrom }) => {
+ 
   return (
     <label className="form__label">
       <span className="form__labelText form__labelText--last">
@@ -11,13 +9,14 @@ export const Input = ({ currencies, amount, setAmount, currencyFrom }) => {
       </span>
       <div className="form__amountVisual">
         <input
+          ref={inputTypedAmount}
           value={amount}
           onChange={({ target }) => setAmount(target.value)}
           className="form__amountVisual-input" 
           type="number" 
           name="amount" 
-          min="1" 
-          step="any" 
+          min="0.01" 
+          step="0.01" 
           placeholder="type here" 
           required
         />
