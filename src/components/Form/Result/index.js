a48437842f1ff127af89fc38baae0e64df92a5e2
Szymon Rojek <sz.rojek@gmail.com>
2021-06-17 10:50:57 +0100

    remove emoji flag function

--- src/components/Form/Result/index.js
+++ src/components/Form/Result/index.js
@@ -1,20 +1,12 @@
 import './style.css';
 
-export const Result = ({ result, getIdCurrencyFrom }) => {
-  
-  const getFlagEmoji = countryCode => 
-    countryCode
-    .toUpperCase()
-    .replace(/./g, char => String.fromCodePoint(127397 + char.charCodeAt()));
-
-  return (
-    <div>
-      <p className="form__text"> 
-        cash in: {result ? result.amount : ''} {result ? getFlagEmoji(getIdCurrencyFrom) : ""}
-      </p>
-      <p className="form__text"> 
-        cash out: {result ? result.value.toFixed(2) : "" } {result ? getFlagEmoji(result.id) : ""}
-      </p>
-    </div>
-  );
-};
+export const Result = ({ result }) => (
+  <div>
+    <p className="form__text"> 
+      cash in: {result ? result.amount : ""} 
+    </p>
+    <p className="form__text"> 
+      cash out: {result ? result.value : "" } 
+    </p>
+  </div>
+);
