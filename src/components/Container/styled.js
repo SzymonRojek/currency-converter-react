27abef6e0ef5d41fa7d5039f55ad152d09e92157
Szymon Rojek <sz.rojek@gmail.com>
2021-06-18 14:37:13 +0100

    add styled components to the Container - rename file, add styles, paths

--- src/components/Container/styled.js
+++ src/components/Container/styled.js
@@ -1,6 +1,9 @@
-.container {
+import styled from "styled-components";
+
+export const StyledContainer = styled.div`
   margin: 0 auto;
   max-width: 550px;
   padding: 0 20px 0 20px;
   color: rgba(255, 255, 255, 0.79);
-}
+`;
+
