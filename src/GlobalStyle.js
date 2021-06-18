44caf835a4af4e3ab599b8c72dce6eda70101459
Szymon Rojek <sz.rojek@gmail.com>
2021-06-18 15:52:06 +0100

    add styles to the index.css and add specific styles for body in the GlobalStyles.js

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -13,4 +13,12 @@ export const GlobalStyle = createGlobalStyle`
   *, ::after, ::before {
     box-sizing: inherit;
   }
+
+  body {
+    min-height: 100vh;
+    font-family: 'Antonio', sans-serif;
+    background-color: #2e86abc4;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
 `;
