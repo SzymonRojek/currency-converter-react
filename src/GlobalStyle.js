6fbbb54867bb404480a0e2415bacd5ae0492359e
Szymon Rojek <sz.rojek@gmail.com>
2021-06-18 13:43:17 +0100

    create GlobalStyle.js file, add global styles and normalize, modify index.js file

---
+++ src/GlobalStyle.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+import { normalize } from 'styled-normalize';
+
+export const GlobalStyle = createGlobalStyle`
+ 
+  ${normalize}
+
+  html {
+    box-sizing: border-box;
+    scroll-behavior: smooth;
+  }
+
+  *, ::after, ::before {
+    box-sizing: inherit;
+  }
+
+  body {
+    min-height: 100vh;
+    font-family: 'Antonio', sans-serif;
+    background-color: #2e86abc4;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
