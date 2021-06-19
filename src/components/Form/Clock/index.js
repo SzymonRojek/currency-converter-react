54024c492200af1ba466dce4e0981e13b2ab3d1a
Szymon Rojek <sz.rojek@gmail.com>
2021-06-19 11:14:51 +0100

    edit Clock component, change functionality

--- src/components/Form/Clock/index.js
+++ src/components/Form/Clock/index.js
@@ -2,27 +2,26 @@ import { ClockBox, StyledText } from './styled';
 import { useState, useEffect } from 'react';
 
 export const Clock = () => {
-  const [date, setDate] = useState(new Date());
-  const [time, setTime] = useState(0);
-
-  const getDate = date.toLocaleDateString(undefined, {
-    weekday : "long",
-    day: "numeric",
-    month: "long",
-  });
-
-  useEffect(() => setTime(date.toLocaleTimeString()), [date]);
+  const [date, setDate] = useState("");
 
   useEffect(() => {
-    const intervalId = setInterval(() => setDate(new Date()), 1000);
-     
+    const intervalId = setInterval(() => {setDate(new Date())}, 1000);
+    
     return () => clearInterval(intervalId);
-  }, [time]);
+  }, []);
 
   return (
     <ClockBox>
       <StyledText>
-        {getDate}, {time}
+       {date.toLocaleString(undefined, 
+       {
+        weekday : "long",
+        day: "numeric",
+        month: "long",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit"
+      })}
       </StyledText>
     </ClockBox>
   );
