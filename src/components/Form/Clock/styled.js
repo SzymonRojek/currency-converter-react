f038028945d9678df8d1e5bc194c9c8791d99461
Szymon Rojek <sz.rojek@gmail.com>
2021-06-19 12:47:31 +0100

    change margin and padding in the Clock component

--- src/components/Form/Clock/styled.js
+++ src/components/Form/Clock/styled.js
@@ -1,14 +1,14 @@
 import styled from 'styled-components'
 
 export const ClockBox = styled.div`
-  margin-top: 50px;
-  padding: 3px 0 3px 0;
+  margin: 50px 0 25px 0;
   text-align: center;
   background-color: #f0f9ff;
   border-radius: 3px;
 `;
 
 export const StyledText = styled.p`
+ padding: 8px 0 8px 0;
   font-size: 20px;
   color: #186889;
   line-height: 1.8;
