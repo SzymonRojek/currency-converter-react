e2f7f660b0fdb187d587068267a665dd548bab6f
Szymon Rojek <sz.rojek@gmail.com>
2021-06-21 12:53:48 +0100

    create custom hoom useCurrentDate - edit Clock component

--- src/components/Form/Clock/index.js
+++ src/components/Form/Clock/index.js
@@ -1,27 +1,13 @@
+import { useCurrentDate } from '../../../useCurrentDate';
 import { ClockBox, StyledText } from './styled';
-import { useState, useEffect } from 'react';
 
 export const Clock = () => {
-  const [date, setDate] = useState(new Date());
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {setDate(new Date())}, 1000);
-    
-    return () => clearInterval(intervalId);
-  }, []);
+  const date = useCurrentDate();
 
   return (
     <ClockBox>
       <StyledText>
-       {date.toLocaleString(undefined, 
-       {
-        weekday : "long",
-        day: "numeric",
-        month: "long",
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit"
-      })}
+        {date}
       </StyledText>
     </ClockBox>
   );
