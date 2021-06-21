39b1e6b362bc98677a8698ee6024794129f35140
Szymon Rojek <sz.rojek@gmail.com>
2021-06-21 23:52:38 +0100

    edit App.js change to arrow function

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { Form } from './components/Form';
 
-export function App() {
-  return (
-    <Container>
-      <Header title="Currency Converter" />
-      <Form />
-      <Footer title="© 2021 | Coded by Szymon Rojek" />
-    </Container>
-  );
-};
+export const App = () => (
+  <Container>
+    <Header title="Currency Converter" />
+    <Form />
+    <Footer title="© 2021 | Coded by Szymon Rojek" />
+  </Container>
+);
