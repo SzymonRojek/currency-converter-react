3d05d0f5641d45e51ffbc8a5b6f4ecce358ce3a6
Szymon Rojek <sz.rojek@gmail.com>
2021-06-22 16:49:38 +0100

    move variable formatted date from the useCurrentDate to index.js

--- src/components/Form/Clock/index.js
+++ src/components/Form/Clock/index.js
@@ -1,13 +1,22 @@
-import { useCurrentDate } from '../../../useCurrentDate';
+import { useCurrentDate } from './useCurrentDate';
 import { Wrapper, TextDate } from './styled';
 
 export const Clock = () => {
   const date = useCurrentDate();
 
+  const formattedDate = date.toLocaleString(undefined, {
+    weekday : "long",
+    day: "numeric",
+    month: "long",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit"
+  });
+
   return (
     <Wrapper>
       <TextDate>
-        {date}
+        {formattedDate}
       </TextDate>
     </Wrapper>
   );
