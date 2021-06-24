654b5ae46197edc5382fc74d893371c1960fadf3
Szymon Rojek <sz.rojek@gmail.com>
2021-06-24 20:21:23 +0100

    Container component export as a styled component too - do not have to index.js and styled.js in this case"

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,7 +1,8 @@
-import { StyledContainer } from './styled';
+import styled from "styled-components";
 
-export const Container = ({ children }) => (
-  <StyledContainer>
-    {children}
-  </StyledContainer>
-);
+export const Container = styled.div`
+  margin: 0 auto;
+  max-width: 550px;
+  padding: 0 20px 0 20px;
+  color: ${({ theme }) => theme.color.primary};
+`; 
--- src/components/Container/styled.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const StyledContainer = styled.div`
-  margin: 0 auto;
-  max-width: 550px;
-  padding: 0 20px 0 20px;
-  color: ${({ theme }) => theme.color.primary};
-`;
-
