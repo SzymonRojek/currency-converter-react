ef9e30cb020fb52878b336d216eddc23aca3fd14
Szymon Rojek <sz.rojek@gmail.com>
2021-07-05 22:15:33 +0100

    add img url to theme, download graph and transfer to images

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,5 @@
+import graph from '../src/images/graph.png'
+
 const colorNames = {
   aliceBlue: "#f0f9ff",
   ebb: "rgba(255, 255, 255, 0.79)",
@@ -24,14 +26,13 @@ export const theme = {
     field: colorNames.trout,
     boxClock: colorNames.aliceBlue,
     eye: colorNames.ebb,
-    mouth: colorNames.ebb,
   },
   button: {
     active: colorNames.tuna,
     backgroundColor: colorNames.trout,
   },
   image: {
-    url: 'https://www.researchgate.net/profile/M-Basri/publication/320925728/figure/fig3/AS:631619933192230@1527601335529/India-GDP-Growth-Interest-Rate-Forex-Reserves-Exchange-Rates-and-Current-Account.png'
+    url: graph,
   },
   accent: {
     boxArrow: colorNames.tuna,
